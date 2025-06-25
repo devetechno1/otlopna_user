33bd5862b73585d94d586ce4a872e4b477ab2770
Mohammed Riyad <riyadm2001@gmail.com>
2025-06-25 16:04:56 +0300

    change image and firebase

--- otlopna_user/web/firebase-messaging-sw.js
+++ otlopna_user/web/firebase-messaging-sw.js
@@ -2,14 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDFN-73p8zKVZbA0i5DtO215XzAb-xuGSE",
-  authDomain: "ammart-8885e.firebaseapp.com",
-  databaseURL: "https://ammart-8885e-default-rtdb.firebaseio.com",
-  projectId: "ammart-8885e",
-  storageBucket: "ammart-8885e.appspot.com",
-  messagingSenderId: "1000163153346",
-  appId: "1:1000163153346:web:4f702a4b5adbd5c906b25b",
-  measurementId: "G-L1GNL2YV61"
+  apiKey: "AIzaSyCot2HfkqBSi3mvZb1VXC9A-g9UHO0xKh0",
+  authDomain: "otlopna.firebaseapp.com",
+  projectId: "otlopna",
+  storageBucket: "otlopna.firebasestorage.app",
+  messagingSenderId: "1039942312396",
+  appId: "1:1039942312396:web:fb58b5bba1a8ef4155bd69",
+  measurementId: "G-HVH45DN3JM"
 });
 
 const messaging = firebase.messaging();
